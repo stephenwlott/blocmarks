1ab19492ad0a364c1a0e281494a7e70d7d8785c3
Stephen Lott <stephenwlott@gmail.com>
2015-07-12 19:45:12 +0000

    mailgun integration3

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -5,17 +5,26 @@ class IncomingController < ApplicationController
 
   def create
     puts "--- IncomingController#create ---"
-
-    
-    # Take a look at these in your server logs
-    # to get a sense of what you're dealing with.
     puts "--- INCOMING PARAMS: #{params.inspect} ---"
-
-    @subject = params[:Subject]
-    puts "--- subject: #{@subject} ---"
     
-    # You put the message-splitting and business
-    # magic here. 
+    # assume that email body consists of url only
+    bookmark_url = params[:body-plain]
+    puts "--- bookmark_url: #{bookmark_url} ---"
+    
+    user_email = params[:sender]
+    puts "--- user_email: #{user_email}"
+    user = User.find_or_create_by!(:email => user_email) do |u|
+      puts "--- created new user for #{user_email}, id = u.id ---"
+    end
+    
+    bookmark_topic = params[:Subject]
+    puts "--- bookmark_topic: #{bookmark_topic} ---"
+    topic = Topic.find_or_create_by!(:title => bookmark_topic, :user_id => user.id) do |t|
+      puts "--- created new topic for #{t.title} ---"
+    end
+
+    # create a new bookmark
+    @bookmark = Bookmark.create!(:url => bookmark_url, :topic_id => topic.id)
 
     # Assuming all went well. 
     head 200
